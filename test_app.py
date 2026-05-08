25492ecb69a895c6b9ab4549966813dfb8171124
tejaravutla287 <tejaravutla287@gmail.com>
2026-05-08 16:59:55 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,4 +1,4 @@
-from app import add,mul,sub
+from app import add,mul,sub,div
  
 def test_add():
     assert add(2, 3) == 5
@@ -6,3 +6,5 @@ def test_mul():
     assert mul(2, 2) == 4
 def test_sub():
     assert sub(2,2)==0
+def test_div():
+    assert div(2,2)==1
